2b69260c45b42af05ae74dcacdf0a7551ae5493d
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2021-07-30 14:36:15 +0300

    added route, switch, Navbar and Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { FC } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { Container } from "@material-ui/core";
-import { Home } from "./views";
+import { About, Contactus, Home, Partners } from "./views";
+import { Navbar, Footer } from "./components";
 
-const App: FC = () => {
-  return (
-    <Container maxWidth="md" fixed>
-      <Home />
-    </Container>
-  );
-};
+const App: FC = () => (
+  <Container maxWidth="md" fixed>
+    <Navbar />
 
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/partners" component={Partners} />
+      <Route path="/contact-us" component={Contactus} />
+      <Route path="/about" component={About} />
+      <Redirect to="/" />
+    </Switch>
+
+    <Footer />
+  </Container>
+);
 export default App;
