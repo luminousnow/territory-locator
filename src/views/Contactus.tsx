6b9097c7147760717c5ece100e13bbed4983cb70
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2021-07-30 14:37:18 +0300

    created Contactus page

---
+++ src/views/Contactus.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { Main } from "../components";
+
+export const Contactus: FC = () => (
+  <Main>
+    <p>
+      Contactus page. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Veritatis dolorum necessitatibus molestias voluptates id eos non inventore
+      ipsam soluta maxime ab corrupti, sequi iste porro quasi cumque recusandae
+      asperiores! Atque?
+    </p>
+  </Main>
+);
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,5 +1,6 @@
 import { Home } from "./Home";
 import { Partners } from "./Partners";
 import { About } from "./About";
+import { Contactus } from "./Contactus";
 
-export { Home, Partners, About };
+export { Home, Partners, Contactus, About };
